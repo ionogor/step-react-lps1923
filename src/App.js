1969cdd12efadde083448aee1c7bef03cf7b875e
step <step@iic.md>
2021-11-16 21:18:30 +0200

    React useState Lecture

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import Post from "./components/Post/Post";
 import postsList from "./data";
+import SearchBar from "./components/SearchBar/SearchBar";
+import { useState } from "react";
 
 function App() {
   //Html + Javascript => JSX - SINTAXA REACT
-  console.log("posts:", postsList);
+  //console.log("posts:", postsList);
+
+  const [posts, setPosts] = useState(postsList);
+  console.log("posts: ", posts);
+
+  const filterPostsByUser = (query) => {
+    const filtredPosts = posts.filter((post) => {
+      return post.username.includes(query);
+    });
+
+    return setPosts(filtredPosts);
+  };
 
   //props-> properties
   return (
     <div className="App">
-      <Post posts={postsList} />
+      <SearchBar filterPostsByUser={filterPostsByUser} />
+      <Post posts={posts} />
     </div>
   );
 }
